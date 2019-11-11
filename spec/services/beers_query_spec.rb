33024fb665ac52255c3bcc56aaa62cbf65355ce7
Jonathan Monsalve <j.jmonsalveg@gmail.com>
2019-11-11 04:31:43 -0600

    test: add new case for query spec

--- spec/services/beers_query_spec.rb
+++ spec/services/beers_query_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe BeersQuery do
   describe '.call' do
 
     context 'without params' do
-      let(:beers) { create_list(:beer, 10)}
+      let(:beers) { create_list(:beer, 10) }
 
       it 'return all' do
         expect(described_class.call({})).to match(beers)
@@ -14,7 +14,7 @@ RSpec.describe BeersQuery do
 
     context 'with  params' do
       let(:canadian_beer) { create(:beer, name: 'Canadian', abv: 2) }
-      let(:extranger_beer) { create(:beer, name: 'extranger', abv: 3 ) }
+      let(:extranger_beer) { create(:beer, name: 'extranger', abv: 3) }
       let(:target_beer) { create(:beer, name: 'Polar', abv: 4.5) }
       let(:target_beer_2) { create(:beer, name: 'Polarcita', abv: 4) }
       let(:target_beers) { [target_beer, target_beer_2] }
@@ -24,14 +24,19 @@ RSpec.describe BeersQuery do
       end
 
       it 'with abv' do
-        expect(described_class.call(abv: 4.5)).to include( have_attributes(id: target_beer.id))
+        expect(described_class.call(abv: 4.5)).to include(have_attributes(id: target_beer.id))
       end
 
       it 'both' do
-        expect(described_class.call(abv: 2, name: 'ca')).to include( have_attributes(id: canadian_beer.id))
+        expect(described_class.call(abv: 2, name: 'ca')).to include(have_attributes(id: canadian_beer.id))
       end
     end
 
-
+    it 'no records in database' do
+      aggregate_failures 'independent from params' do
+        expect(described_class.call(abv: 2, name: 'ca')).to match_array Beer.none
+        expect(described_class.call({})).to match_array Beer.none
+      end
+    end
   end
 end
