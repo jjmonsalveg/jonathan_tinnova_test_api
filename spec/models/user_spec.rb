7b65a89dda7b667f1637f388c7830302c855cb56
Jonathan Monsalve <j.jmonsalveg@gmail.com>
2019-11-11 02:02:01 -0600

    refactor: change should notation by expect in user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it { should validate_uniqueness_of(:email) }
-    it { should validate_uniqueness_of(:username) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:username) }
   end
 end
